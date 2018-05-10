25abc33dc3deaccca797e10416af7cdd86038ebb
wellcam <wel.camilo10@gmail.com>
2018-05-10 02:09:56 -0300

    Criadas as telas de Cadastro, Login e botões

--- app/src/main/java/com/anhembi/gabaritando/gabaritando/Login.java
+++ app/src/main/java/com/anhembi/gabaritando/gabaritando/Login.java
@@ -1,13 +1,34 @@
 package com.anhembi.gabaritando.gabaritando;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+    private Button btnCadastro, btnAlunos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnCadastro = (Button) findViewById(R.id.btnCadastro);
+        btnAlunos = (Button) findViewById(R.id.btnAlunos);
+
+        btnCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Cadastro.class));
+            }
+        });
+
+        btnAlunos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Alunos.class));
+            }
+        });
     }
 }
